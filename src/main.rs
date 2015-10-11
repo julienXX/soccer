5a61189498f8ba49f59f0c41fc4caedcfbae7ca0
Julien Blanchard <julien@sideburns.eu>
2015-10-11 15:01:23 +0200

    Display a TeamRanking

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,26 @@ use std::env;
 use hyper::Client;
 use hyper::header::Connection;
 
-use rustc_serialize::json::{Json};
+use rustc_serialize::json::{self, Json, Decoder, Object};
 
 const API_URL: &'static str = "http://api.football-data.org/alpha/soccerseasons/";
+
+ #[derive(Debug, RustcDecodable)]
+pub struct TeamRanking {
+    position: u8,
+    teamName: String,
+    playedGames: u8,
+    points: u8,
+    goals: u8,
+    goalsAgainst: u8,
+    goalDifference: u8
+}
 
 fn main() {
     match env::args().nth(1) {
         Some(league) => {
             let (name, ranking) = get_ranking(league);
-            println!("{}\n\n{}", name, ranking);
+            println!("{}\n\n{:?}", name, ranking);
         }
         None => {
             println!("Usage: soccer <league>");
@@ -24,7 +35,7 @@ fn main() {
     };
 }
 
-fn get_ranking(league: String) -> (String, String) {
+fn get_ranking(league: String) -> (String, TeamRanking) {
     let url = API_URL.to_string() + &league.to_string() + "/leagueTable";
     let client = Client::new();
 
@@ -38,7 +49,9 @@ fn get_ranking(league: String) -> (String, String) {
     let json_body = Json::from_str(&body).unwrap();
     let json_object = json_body.as_object().unwrap();
     let caption = json_object.get("leagueCaption").unwrap();
-    let ranking = json_object.get("standing").unwrap();
+    let ranking = json_object.get("standing").unwrap()[0].to_string();
 
-    (caption.to_string(), ranking.to_string())
+    let team_ranking: TeamRanking = json::decode(&ranking).unwrap();
+
+    (caption.to_string(), team_ranking)
 }
